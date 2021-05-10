398c7bc5a11447fb34fa4e530c75ad86fb5f5ad4
Travis Severson <72591899+Travis-Severson70@users.noreply.github.com>
2021-05-10 06:29:16 -0700

    cleaned up code, added picture, renamed logo to be correct size

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Welcome to my Portfolio page!")
+console.log('Welcome to my Portfolio page!');
 
 function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
   } else {
-    x.className = "navtoggle";
+    x.className = 'navtoggle';
   }
 }
